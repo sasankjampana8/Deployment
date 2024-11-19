bca58835d87116264bc59990a9ef6d8464b4ed8c
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-19 10:12:55 +0530

    updates

--- telecom_churn_prediction/inference/prediction.py
+++ telecom_churn_prediction/inference/prediction.py
@@ -1,11 +1,29 @@
 import pandas as pd 
 import os
 import pickle as pkl 
-
+from preprocess import Preprocess
 
 
 class Prediction:
-    def __init__(self) -> None:
+    def __init__(self, model_path) -> None:
         #load the model and estimators
         # self.model = pkl.load(open(model_path, 'rb'))
-        self.model = None
+        self.model_path = model_path
+        try:
+            for file in os.listdir(self.model_path):
+                if "model.pkl" == file:
+                    self.model = pkl.load(open(os.path.join(self.model_path, file), 'rb'))
+                    print("Model Loaded..")
+                    break
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            
+    
+    def preprocess(self, data):
+        #preprocess the data
+        process = Preprocess(self.model_path)
+        
+    
+    def make_prediction(self,data ):
+        #make prediction
+        processed_data = self.preprocess(data)
--- telecom_churn_prediction/inference/preprocess.py
+++ telecom_churn_prediction/inference/preprocess.py
@@ -1,10 +1,23 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+import pickle as pkl
+import os
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 
 class Preprocess:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model_path):
+        self.model_path = model_path  # Assign the argument to an instance variable
+        self.estimators = dict()  # Dictionary of estimators
+        self.msg = []
+        try:
+            for file in os.listdir(self.model_path):
+                if "pkl" in file:
+                    self.estimators[file.split(".")[0]] = pkl.load(open(os.path.join(self.model_path, file), 'rb'))
+                elif "model" in file:
+                    continue
+            print("Loaded the estimators...")
+        except Exception as e:
+            print(f"Error loading the estimators: {e}")
     
     def process(self, data):
         pass
